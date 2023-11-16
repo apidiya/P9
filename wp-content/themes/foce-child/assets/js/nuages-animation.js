8216a4adbc1b2ff58758f8068e4028487093db47
Apidiya <128754190+apidiya@users.noreply.github.com>
2023-11-16 19:21:55 +0100

    responsive a finir

--- wp-content/themes/foce-child/assets/js/nuages-animation.js
+++ wp-content/themes/foce-child/assets/js/nuages-animation.js
@@ -1,20 +1,27 @@
 console.log(" le script nuages-animations.js est lancé")
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Fonction pour déplacer les nuages au scroll
-    function scrollNuage() {
-        const scrollY = window.scrollY;
-        const NuagesBleu = document.getElementById('NuagesBleu');
-
-        // Parametre pour régler les préférences (Position nuage et Vitesse de déplacement)
-        const NuagesBleuVitesse = 0.3;
-        const NuagesBleuPosition = 300;
-
-        // Nouvelles positions nuage en fonction du défilement
-        const NuagesBleuX = -scrollY * NuagesBleuVitesse + NuagesBleuPosition;
-        NuagesBleu.style.transform = `translateX(${NuagesBleuX}px)`;
-    }
-
-    window.addEventListener('load', scrollNuage); // Déclencher au chargement de la page
-    window.addEventListener('scroll', scrollNuage); // Déclencher à chaque défilement
-});
+document.addEventListener("DOMContentLoaded", function() {
+    var nuagesBleu = document.getElementById("NuagesBleu");
+    var sectionTitle = document.querySelector(".story #place h3");
+  
+    window.addEventListener("scroll", function() {
+      var boundingBox = sectionTitle.getBoundingClientRect();
+  
+      // Vérifier si le titre h3 est visible
+      if (boundingBox.top < window.innerHeight && boundingBox.bottom >= 0) {
+        // Calculer la translation en fonction du défilement
+        var scrollPercentage = boundingBox.bottom / window.innerHeight;
+        var maxTranslation = 300;
+        var translation = maxTranslation * (1 - scrollPercentage);
+  
+        // Limiter la translation à 300px au maximum
+        translation = Math.min(maxTranslation, translation);
+  
+        // Appliquer la translation à #NuagesBleu
+        nuagesBleu.style.transform = "translateX(-" + translation + "px)";
+      } else {
+        // Si le titre n'est pas visible, revenir à la position initiale
+        nuagesBleu.style.transform = "translateX(0)";
+      }
+    });
+  });
